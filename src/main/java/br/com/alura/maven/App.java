ef6a7ad5508a335156acd942bdf2728cb69d18c9
CarlosAmaral <CarlosAmaral@HERMES.icarotech.com>
2020-12-15 21:00:28 -0300

    Escrita de testes para a classe Product e adicionando dependências ao pom.xml

--- src/main/java/br/com/alura/maven/App.java
+++ src/main/java/br/com/alura/maven/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Aprendendo a usar o Maven CORRETAMENTE!" );
+        
+        Product myProduct = new Product("Nintendo Switch", 2999.0);
+        
+        System.out.println(myProduct);
     }
 }
